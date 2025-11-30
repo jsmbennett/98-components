a58896e80e513e3a7d6c6737fd95e02a06a85078
Joseph Bennett <joeyb@Josephs-MacBook-Air.local>
2025-11-30 07:50:24 -0800

    More overrides

--- src/components/button.js
+++ src/components/button.js
@@ -1,34 +1,34 @@
 import win98Styles from '98.css?inline';
 
 class Win98Button extends HTMLElement {
-    constructor() {
-        super();
-        this.attachShadow({ mode: 'open' });
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  static get observedAttributes() {
+    return ['default', 'disabled'];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (this.shadowRoot) {
+      this.render();
     }
+  }
 
-    connectedCallback() {
-        this.render();
-    }
+  render() {
+    const isDefault = this.hasAttribute('default');
+    const isDisabled = this.hasAttribute('disabled');
 
-    static get observedAttributes() {
-        return ['default', 'disabled'];
-    }
+    const sheet = new CSSStyleSheet();
+    sheet.replaceSync(win98Styles);
+    this.shadowRoot.adoptedStyleSheets = [sheet];
 
-    attributeChangedCallback(name, oldValue, newValue) {
-        if (this.shadowRoot) {
-            this.render();
-        }
-    }
-
-    render() {
-        const isDefault = this.hasAttribute('default');
-        const isDisabled = this.hasAttribute('disabled');
-
-        const sheet = new CSSStyleSheet();
-        sheet.replaceSync(win98Styles);
-        this.shadowRoot.adoptedStyleSheets = [sheet];
-
-        this.shadowRoot.innerHTML = `
+    this.shadowRoot.innerHTML = `
       <style>
         :host {
           display: inline-block;
@@ -45,21 +45,21 @@ class Win98Button extends HTMLElement {
       </button>
     `;
 
-        this.setupInteractions();
-    }
+    this.setupInteractions();
+  }
 
-    setupInteractions() {
-        const button = this.shadowRoot.querySelector('button');
+  setupInteractions() {
+    const button = this.shadowRoot.querySelector('button');
 
-        button.addEventListener('click', (e) => {
-            if (!this.hasAttribute('disabled')) {
-                this.dispatchEvent(new CustomEvent('button-click', {
-                    bubbles: true,
-                    composed: true
-                }));
-            }
-        });
-    }
+    button.addEventListener('click', (e) => {
+      if (!this.hasAttribute('disabled')) {
+        this.dispatchEvent(new CustomEvent('button-click', {
+          bubbles: true,
+          composed: true
+        }));
+      }
+    });
+  }
 }
 
 customElements.define('win98-button', Win98Button);

e479890a62b83e198e334efe2dc66d890382d561
Joseph Bennett <joeyb@Josephs-MacBook-Air.local>
2025-12-02 21:54:36 -0800

    Begin work on Start Menu, determine better sidebar font

--- src/index.js
+++ src/index.js
@@ -6,5 +6,6 @@ export { default as Win98Button } from './components/button.js';
 export { default as Win98Select } from './components/select.js';
 export { default as Win98Desktop } from './components/desktop.js';
 export { default as Win98Taskbar } from './components/taskbar.js';
+export { default as Win98StartMenu } from './components/startmenu.js';
 
 export { windowManager } from './services/WindowManager.js';
